4a8ddf88c24bed2850b135517d1dad94d108c1b9
Ian Moore <mooremathut@gmail.com>
2018-05-11 14:21:42 -0500

    dashspanish commit 2, playing around with ui and dashboard options

--- dashspanish/server.R
+++ dashspanish/server.R
@@ -1,11 +1,69 @@
 
 
+messageData <-data.frame(from=c("ian","ben","gabby","cat"),
+                         message=c("fuck","shit","ass","dick"),
+                         # icon=c(paste0(icon("question")),paste0(icon("question")),
+                         #        paste0(icon("life-ring")),paste0(icon("life-ring"))
+                         #        )
+                         icon=c("question","question","life-ring","life-ring")
+                         )
 
+# taskData <- data.frame()
+# notificationData <- data.frame()
+# taskItem(text = row[["message"]], color = row[["color"]], value = row[["value"]])
+# notificationItem(text = row[["message"]], status = row[["status"]])
 
 server <- function(input, output, session) {
   
   
   output$mtcars <- renderTable(head(mtcars))
+  
+  
+  output$messageMenu <- renderMenu({
+    msgs <- apply(messageData, 1, function(row) {
+      messageItem(from = row[["from"]], message = row[["message"]])})
+    
+    dropdownMenu(type = "messages", .list = msgs)
+    
+  })
+  
+  output$menu <- renderMenu({
+    sidebarMenu(
+      sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
+                        label = "Search..."),
+      tags$br(),
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Widgets", icon = icon("th"), tabName = "widgets"),
+      
+      menuItem('y', tabName = 'y',
+               icon = icon('line-chart'),
+               menuItem('a',
+                        tabName = 'a',
+                        icon = icon('line-chart')),
+               menuItem('b',
+                        tabName = 'b',
+                        icon = icon('line-chart'),
+                        menuSubItem('l',
+                                    tabName = 'l',
+                                    icon = icon('line-chart')),
+                        menuSubItem('m',
+                                    tabName = 'm',
+                                    icon = icon('line-chart'))),
+               menuItem('c',
+                        tabName = 'c',
+                        icon = icon('line-chart')))
+      
+      
+    )
+  })
+  
+  
+  
+  
+  
+  
+  
+  
 
   
   
--- dashspanish/ui.R
+++ dashspanish/ui.R
@@ -1,20 +1,47 @@
 
 
 
-shinyUI(
-  dashboardPage(
-  
-  dashboardHeader(title = "Quick Example"),
-  dashboardSidebar(textInput("text", "Text")),
-  dashboardBody(
-    valueBox(100, "Basic example"),
-    tableOutput("mtcars")
-    
-    
-    )
-  
-  
-  
-  )  
-)
 
+
+header <- source("ui/header.R",local = T)$value
+
+
+
+sidebar <- dashboardSidebar(sidebarMenuOutput("menu"))
+
+
+
+
+body <- dashboardBody(
+  
+  tabItems(
+    
+    tabItem(tabName = "dashboard",
+            box(
+              title = "Data box", status = "primary", solidHeader = TRUE,
+              collapsible = TRUE,
+              h3("Hello")
+            )
+    ),
+    
+    tabItem(tabName = "widgets",
+            box(
+              title = "Data Box", status = "primary", solidHeader = TRUE,
+              collapsible = TRUE,
+              h3("Whats up")
+            )
+    )
+    
+  )
+  
+)
+  
+
+
+
+
+
+
+
+
+shinyUI(dashboardPage(header, sidebar, body))
